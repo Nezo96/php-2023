b3dc3f7b2322fc74061ae119451fc0124dbd4cbf
Nezo96 <racradoslav96@gmail.com>
2023-04-14 20:16:33 +0200

    základné dátové typy

---
+++ 05základne-datove-typy.php
@@ -0,0 +1,15 @@
+<?php
+
+    // Základné dátové typy
+    $school_name = "Levice"; // text - string
+    var_dump($school_name);
+
+    echo "<br>";
+
+    $number_of_students = 980;  // celé číslo - integer
+    var_dump($number_of_students);
+
+    echo "<br>";
+
+    $average_of_school = 3.1; // desatinné číslo - float
+    var_dump($average_of_school);
