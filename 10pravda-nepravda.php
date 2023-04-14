b5822eea1459b9825e95d898304871195a565ba2
Nezo96 <racradoslav96@gmail.com>
2023-04-14 21:39:15 +0200

    pravda | nepravda

---
+++ 10pravda-nepravda.php
@@ -0,0 +1,11 @@
+<?php
+
+    // Pravda, nepravda | true, false
+    $internat = false; // nepravda | nič sa nevypíše
+    $adult = true; // pravda | 1
+
+    $is_logged = true;
+    $database_connection = false;
+    echo "Prihlásenie užívatela: $is_logged";
+    echo "<br>";
+    echo "Napojenie do databáze: $database_connection";
