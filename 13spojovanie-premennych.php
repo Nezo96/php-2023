606a8acd8d64ec515c29dc1525528b895b39f583
Nezo96 <racradoslav96@gmail.com>
2023-04-14 21:53:12 +0200

    spojovanie premenných

---
+++ 13spojovanie-premennych.php
@@ -0,0 +1,11 @@
+<?php
+
+    // Spojovanie premenných
+    $first_name = "Harry";
+    $second_name = "Potter";
+    $friend_first_name = "Ron";
+
+    // V PHP sa k spojovaniu premenných používa bodka
+    echo $first_name . " " . $second_name;
+    echo "<br>";
+    echo $first_name . " a " . $friend_first_name;
