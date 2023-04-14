2e3d22a6972a804a0fb42175d68c7145f3fd04df
Nezo96 <racradoslav96@gmail.com>
2023-04-14 22:19:39 +0200

    polia

---
+++ 17polia-array.php
@@ -0,0 +1,24 @@
+<?php
+
+    // Pole (array)
+    $student_1 = "Harry";
+    $student_2 = "Hermiona";
+    $student_3 = "Ron";
+
+    // Vytvorenie pola s hodnotami
+    $students = ["Harry", "Ron", "Hermiona"];
+
+    // echo $student_1;
+    var_dump($students);
+    echo "<br>";
+
+    // Pole vždy začína od 0
+    // K hodnotám v poli sa pristupuje pomocov indexov
+    echo $students[0];
+    echo "<br>";
+
+    echo $students[1];
+    echo "<br>";
+
+    echo $students[2];
+    echo "<br>";
