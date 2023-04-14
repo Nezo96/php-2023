14baf35f16baab115ef8d8174238506e9ba66d62
Nezo96 <racradoslav96@gmail.com>
2023-04-14 20:04:05 +0200

    vytvorenie premennych

---
+++ 04tvorba-premennych.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Levice";
+    echo "<br>";
+    echo "Levice";
+    echo "<br>";
+    echo "Levice";
+    echo "<br>";
+    echo "---------------------";
+    echo "<br>";
+
+    // Vytvorenie premmenj
+    $moja_premenna = "Nová škola";
+
+    // Vypísanie premmenej
+    echo $moja_premenna;
+    echo "<br>";
+    echo $moja_premenna;
