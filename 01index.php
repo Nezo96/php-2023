f75a922f259ba6471916f9133d6f225225a694ab
Nezo96 <racradoslav96@gmail.com>
2023-04-14 19:49:13 +0200

    Initial commit

--- 01index.php
+++ 01index.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-   <h1>Základ</h1> 
+   <h1>Základ test</h1> 
 </body>
 </html>
---
+++ 02Vypsanie-obsahu-do-stranky.php
@@ -0,0 +1,20 @@
+<?php
+
+//Text sa vypisuje pomocou 'echo'
+//Vždy treba ukončiť php zápis s bodkočiarkou ;
+
+echo "Prvý vypísaný text";
+
+// Čísla nevypisovať do uvodzoviek
+echo 100;
+
+// Dajú sa vypisovať aj HTML znaky
+echo "<br>";
+echo "Levice";
+
+// Komentár na jeden riadok
+# Taktiež komentár na jeden riadok
+/* Komentar
+ na
+ viacej
+ riadokok */
