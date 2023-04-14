02e256fe2e71bfba68f05faa8f25285b9064177c
Nezo96 <racradoslav96@gmail.com>
2023-04-14 20:41:58 +0200

    externe odkazy

---
+++ 07externe-odkazy.php
@@ -0,0 +1,27 @@
+<?php
+
+    // Odkaz
+    echo "<a href='https://www.youtube.com/'>YouTube</a>";
+
+    echo "<br>";
+
+    // Odkaz
+    echo '<a href="https://www.youtube.com/">YouTube</a>';
+
+    echo "<br>";
+
+    // Odkaz
+    echo "<a href=\"https://www.youtube.com/\">YouTube</a>";
+
+    echo "<br>";
+
+    // Odkaz
+    $link_text = "YouTube";
+    echo "<a href='https://www.youtube.com/'>$link_text</a>";
+
+    echo "<br>";
+
+    // Odkaz
+    $link_address = "https://www.youtube.com/";
+    echo "<a href=$link_address>$link_text</a>";
+?>
