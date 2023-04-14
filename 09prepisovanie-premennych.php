79b097a115eeb23aab14485aeb5c369856931b5a
Nezo96 <racradoslav96@gmail.com>
2023-04-14 21:32:30 +0200

    prepisovanie premenných

---
+++ 09prepisovanie-premennych.php
@@ -0,0 +1,11 @@
+<?php
+
+    // Prepisovanie premenných (statické a dynamické typovanie)
+    // PHP má dynamické typovanie, typuje sa až podľa koncovej hodnoty
+
+    // Najprv string
+    $name = "Rado";
+
+    // Potom číslo
+    $name = 27;
+    echo $name;
