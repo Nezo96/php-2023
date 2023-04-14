9e2ad9830c3d7033146a532ae2a93f68fb837921
Nezo96 <racradoslav96@gmail.com>
2023-04-14 19:56:42 +0200

    druhy vypisovanie

---
+++ 03vypisane-pomocou-var_dump.php
@@ -0,0 +1,12 @@
+<?php
+
+// Klasický výpis
+echo "Klasický výpis";
+echo "<br>";
+
+// Výpis pomocou var_dump - výpis informácii
+var_dump("Levice");
+echo "<br>";
+var_dump(100);
+echo "<br>";
+var_dump(58.9);
