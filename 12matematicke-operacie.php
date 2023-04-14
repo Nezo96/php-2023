3e595fb2f612321d31b2f4430feeb38bb5557450
Nezo96 <racradoslav96@gmail.com>
2023-04-14 21:47:57 +0200

    matematické operácie

---
+++ 12matematicke-operacie.php
@@ -0,0 +1,33 @@
+<?php
+
+    // Matematické operácie
+    $students_2022 = 100;
+    $students_2023 = 124;
+
+    // Odčítanie
+    $result = $students_2023 - $students_2022;
+
+    // Sčítanie
+    $result2 = $students_2023 + $students_2022;
+
+    // Násobenie
+    $result3 = $students_2023 * $students_2022;
+
+    // Delenie
+    $result4 = $students_2023 / $students_2022;
+
+    // Zátvorky
+    $result5 = ($students_2023 / $students_2022) * 5;
+
+    // Nulou sa nedá deliť v PHP
+    
+    echo "Výsledok: $result";
+    echo "<br>";
+    echo "Výsledok: $result2";
+    echo "<br>";
+    echo "Výsledok: $result3";
+    echo "<br>";
+    echo "Výsledok: $result4";
+    echo "<br>";
+    echo "Výsledok: $result5";
+    echo "<br>";
