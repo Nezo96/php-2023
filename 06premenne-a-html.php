6286e706324b0e4147e6a8d4331de3fe501aa7d7
Nezo96 <racradoslav96@gmail.com>
2023-04-14 20:31:36 +0200

    premenne a html

---
+++ 06premenne-a-html.php
@@ -0,0 +1,11 @@
+<?php
+
+    // Premmené a HTML
+    $school_name = "Bradavice";
+    echo "<h1>$school_name</h1>";
+
+    $sub_heading = "Škola čarov a kúziel";
+    echo "<h2>$sub_heading</h2>";
+
+    $paragraph = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam adipisci aut beatae esse mollitia, saepe cum voluptatem, totam aliquid ab iure quisquam nam non laudantium provident, deleniti at odit modi!";
+    echo "<p>$paragraph</p>";
