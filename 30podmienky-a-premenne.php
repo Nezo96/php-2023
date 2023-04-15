1d794c983c55b633da4d8acab8f52d10ea6d6a3c
Nezo96 <racradoslav96@gmail.com>
2023-04-15 12:46:13 +0200

    podmienky a premenné

---
+++ 30podmienky-a-premenne.php
@@ -0,0 +1,10 @@
+<?php
+
+    // Podmienky a premenné
+    $college = "Zmijozel";
+
+    if ($college == "Chrabromil") {
+        echo "Vstúpte";
+    } else {
+        echo "Nemáte oprávnenie na vstup";
+    }
