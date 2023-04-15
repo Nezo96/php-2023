0a763a112f46f1eac63f3ce7de0adff6a6d1cd9a
Nezo96 <racradoslav96@gmail.com>
2023-04-15 12:45:51 +0200

    podmienky

---
+++ 29podmienky.php
@@ -0,0 +1,10 @@
+<?php
+
+    // Podmienky (typ: boolean)
+    // Ak podmienka platí vykoná sa to čo je vo vnútry if
+    // Ak podmienka neplatí vykoná sa to čo je v else 
+    if (5 > 2) {
+        echo "Ano to je pravda";
+    } else {
+        echo "Nie nie je to pravda";
+    }
